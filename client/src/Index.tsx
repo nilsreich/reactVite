bdf4bbe91bccadccb49a6ea602894ae3664261fa
nilsreich <nilsreich@gmail.com>
2023-12-17 14:51:28 +0000

    init

---
+++ client/src/Index.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from "./components/ModeToggle"
+
+export const Index=()=>{
+    return (
+        <div>
+            <ModeToggle />
+            asd
+        </div>
+    )
+}
---
+++ client/src/main.tsx
@@ -0,0 +1,61 @@
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  Outlet,
+  RouterProvider,
+  Link,
+  Router,
+  Route,
+  RootRoute,
+} from "@tanstack/react-router";
+import { Index } from "./Index";
+import { About } from "./About";
+import { ThemeProvider } from "./components/theme-provider";
+
+const rootRoute = new RootRoute({
+  component: () => (
+    <>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>{" "}
+        <Link to="/about" className="[&.active]:font-bold">
+          About
+        </Link>
+      </div>
+      <hr />
+      <Outlet />
+    </>
+  ),
+});
+
+const indexRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "/",
+  component: Index,
+});
+
+const aboutRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "/about",
+  component: About,
+});
+
+const routeTree = rootRoute.addChildren([indexRoute, aboutRoute]);
+
+const router = new Router({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </React.StrictMode>
+);
